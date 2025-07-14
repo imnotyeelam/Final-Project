65ac2514f2a941eb44df4b55942bf204c79a3af3
XINYU LIN <crystallinxinyu@outlook.com>
2025-07-14 23:51:34 +0800

    111

--- Assets/Level3-Scripts/L3PlayerController.cs
+++ Assets/Level3-Scripts/L3PlayerController.cs
@@ -1,24 +1,37 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class L3PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 5f;
     public CharacterController charCon;
+    public Transform cameraTransform; //拖入你的主相机
 
-    private Vector3 moveInput;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float mouseSensitivity;
 
-    // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
 
-        charCon.Move(moveInput);
+        // 获取摄像机朝向的 forward 和 right，但去掉垂直分量
+        Vector3 camForward = cameraTransform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = cameraTransform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        // 基于摄像机的方向来移动
+        Vector3 moveDir = camRight * inputX + camForward * inputZ;
+
+        charCon.Move(moveDir * moveSpeed * Time.deltaTime);
+
+        //camera rotation control look left&right
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        cameraTransform.rotation = Quaternion.Euler(cameraTransform.rotation.eulerAngles.x - mouseInput.y, cameraTransform.rotation.eulerAngles.y, cameraTransform.rotation.eulerAngles.z);
     }
 }
