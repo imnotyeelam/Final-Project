30af5e2dd1f84cf594b98565315c87a29d71830f
lunatic-d <chanyikka03@gmail.com>
2025-07-15 14:36:06 +0800

    如

--- Assets/Scripts/GunShooter.cs
+++ Assets/Scripts/GunShooter.cs
@@ -1,15 +1,14 @@
 using UnityEngine;
-using DigitalRuby.LightningBolt; // Namespace from the asset
 
 public class GunShooter : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public GameObject lightningPrefab; // prefab that has LightningBoltScript
+    public LightningEffect lightningPrefab;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) // Left click
         {
             Shoot();
         }
@@ -17,18 +16,12 @@ public class GunShooter : MonoBehaviour
 
     void Shoot()
     {
-        // Fire bullet
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        // Instantiate bullet
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-        // Trigger lightning
-        GameObject lightningGO = Instantiate(lightningPrefab);
-        LightningBoltScript lightning = lightningGO.GetComponent<LightningBoltScript>();
-        if (lightning != null)
-        {
-            lightning.StartObject = firePoint.gameObject;
-            lightning.EndObject = null; // optional, use EndPosition if targeting air
-            lightning.StartPosition = firePoint.position;
-            lightning.EndPosition = firePoint.position + firePoint.forward * 10f;
-        }
+        // Instantiate lightning
+        LightningEffect lightning = Instantiate(lightningPrefab);
+        Vector3 endPoint = firePoint.position + firePoint.forward * 5f;
+        lightning.ShowLightning(firePoint.position, endPoint);
     }
 }
