bf9b6b689d2913c74e46da20386a475e7b4b7a2c
imnotyeelam <lam.chia41@gmail.com>
2025-07-15 11:42:42 +0800

    task panel done

--- Assets/TaskPanelGroup.cs
+++ Assets/TaskPanelGroup.cs
@@ -5,30 +5,22 @@ public class TaskPanelGroup : MonoBehaviour
 {
     public RectTransform taskPanel; 
     public Vector2 shownPos;
-    public Vector2 hiddenPos; 
-    private bool isShown = false;
+    public Vector2 hiddenPos;
+
+    private bool isShown = true;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                Debug.Log("Hit object: " + hit.collider.name);
-
-                if (hit.collider.CompareTag("TaskToggleButton"))
-                {
-                    Debug.Log("Toggle Button clicked!");
-                    TogglePanel();
-                }
-            }
+            TogglePanel();
         }
     }
 
     void TogglePanel()
     {
         isShown = !isShown;
-        taskPanel.DOAnchorPos(isShown ? shownPos : hiddenPos, 0.5f).SetEase(Ease.OutCubic);
+        taskPanel.DOAnchorPos(isShown ? shownPos : hiddenPos, 0.5f)
+                 .SetEase(Ease.OutCubic);
     }
 }
