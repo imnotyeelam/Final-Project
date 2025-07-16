4bb106d6e2b923aa14cde1bb39987fcad5bfeace
XINYU LIN <crystallinxinyu@outlook.com>
2025-07-16 18:07:11 +0800

    111

---
+++ Assets/Level3-Scripts/Turret.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public float rangeToTargetPlayer, timeBetweenShots = 0.5f;
+    private float shotCounter;
+
+    public Transform gun, firePoint;
+
+    public float rotateSpeed = 45f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        shotCounter = timeBetweenShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, L3PlayerController.instance.transform.position) < rangeToTargetPlayer)
+        {
+            gun.LookAt(L3PlayerController.instance.transform.position + new Vector3(0f, 0.2f, 0f));
+
+            shotCounter -= Time.deltaTime;
+
+            if (shotCounter <= 0)
+            {
+                Instantiate(bullet, firePoint.position, firePoint.rotation);
+                shotCounter = timeBetweenShots;
+            }
+        }
+
+        else
+        {
+            shotCounter = timeBetweenShots;
+            gun.rotation = Quaternion.Lerp(gun.rotation, Quaternion.Euler(0f, gun.rotation.eulerAngles.y + 10f, 0f), rotateSpeed * Time.deltaTime);
+        }
+    }
+}
