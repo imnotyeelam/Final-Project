777a516951455d806ac1928b7a6eb094dcdd1bcf
XINYU LIN <crystallinxinyu@outlook.com>
2025-07-19 12:19:01 +0800

    1111

--- Assets/Level3-Scripts/Turret.cs
+++ Assets/Level3-Scripts/Turret.cs
@@ -11,6 +11,23 @@ public class Turret : MonoBehaviour
 
     public float rotateSpeed = 45f;
 
+    public LayerMask obstacleMask; // 设置障碍层（比如 Bookcase）
+    bool CanSeePlayer()
+    {
+        Vector3 direction = (PlayerController1.instance.transform.position - firePoint.position).normalized;
+        float distance = Vector3.Distance(firePoint.position, PlayerController1.instance.transform.position);
+
+        // 射线检测
+        if (Physics.Raycast(firePoint.position, direction, out RaycastHit hit, distance, ~0))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                return true; // 中间没有障碍物，可以攻击
+            }
+        }
+        return false; // 有障碍挡住
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,7 +37,7 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, PlayerController1.instance.transform.position) < rangeToTargetPlayer)
+        if (Vector3.Distance(transform.position, PlayerController1.instance.transform.position) < rangeToTargetPlayer && CanSeePlayer())
         {
             gun.LookAt(PlayerController1.instance.transform.position + new Vector3(0f, 0.2f, 0f));
 
